ceb42d542f648bb72665b9b0817f03efb924f0e9
cheesepizza453 <71148177+cheesepizza453@users.noreply.github.com>
2020-09-23 00:03:26 +0900

    포트폴리오.v.0.0.1
    200923 섹션 별 타이틀 미완!

--- js/main.js
+++ js/main.js
@@ -1,63 +1,89 @@
 $(document).ready(function (){
 
-  $('.slide_area').slick({
-    slidesToShow: 1,
-    dots: true,
-    arrows: true,
-    infinite: true,
-    centerMode: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    centerPadding: '0'
+  $(window).scroll(function(){
+
+    var mt = 'margin-top';
+    var ml = 'margin-left';
+    var mr = 'margin-right';
+    var to = 'transform-origin';
+
+    var position = $(window).scrollTop();
+
+
+    if(position > 0 && position < 200 ) {
+
+      $('.introduce .content_area').css({top: '-100vh'});
+      // $('.top .title_area').css({
+      //   position: 'fixed',
+      //   width: '100%',
+      //   height: 'auto',
+      //   top: '12vh',
+      //   left: '50%',
+      //   transform:'translateX(-50%)',
+      //   transition: 'top 1s, left 0.8s, transform 0.8s, margin 0.81s',
+      //   margin: '0 0 0 -430px'
+      //})
+
+
+    } else if (position > 200 && position < 1200 ) {
+      $('.introduce .content_area').css({top: '0vh'});
+      // $('.top .title_area').css({
+      //   position: 'fixed',
+      //   margin: '0',
+      //   top:'30px',
+      //   left: '30px',
+      //   transform:'translateX(0%) scale(0.3)',
+      //   width: '100%',
+      //   height: 'auto',
+      //   transition: 'top 0.8s, left 0.8s, transform 0.8s, margin 0.81s',
+      //   margin: '0'
+      //})
+
+
+
+
+    } else if (position > 1200 && position < 1800) {
+
+      // $('body').css('background', 'red');
+      $('.block2').css({ opacity: '0' });
+      $('.block3').css({ opacity: '1' });
+
+      $('.stick1, .stick2, .stick3, .stick4').css('background', 'magenta');
+
+      $('.stick1').css({
+        transform:'rotate(15deg)',
+        left: '84px',
+        top: '-45px',
+        height: '630px'
+      });
+      $('.stick2').css({
+        transform:'rotate(-40deg)',
+        left: '335px',
+        top: '-105px',
+        height: '790px'
+      });
+      $('.stick3').css({
+        transform:'rotate(-15deg)',
+        left: '467px',
+        top: '-27px',
+        height: '650px'
+      });
+      $('.stick4').css({
+        transform:'rotate(-50deg)',
+        left: '480px',
+        top: '-50px',
+        height: '375px'
+      });
+
+    }
+
   });
 
+  $('.stick1, .stick2, .stick3, .stick4').mouseenter(function(){
+				$('.stick1, .stick2, .stick3, .stick4').addClass('flash'); }).mouseleave(function() {
+					$('.stick1, .stick2, .stick3, .stick4').removeClass('flash');
+	});
 
-  $('a[href="#"]').click(function(e) {
-    e.preventDefault();
-  });
 
 
-  $('.ch_list').click(function(){
-      $('.ch_list').removeClass('active');
-      $(this).addClass('active');
-
-      var index = $(this).index();
-
-      if(index == 0){
-        $('.all_channel_area .channel').css('display','block');
-      }else if (index >= 1){
-      $('.all_channel_area .channel').css('display','none');
-      $('.channel').eq(index*2-1).css('display','block');
-      $('.channel').eq(index*2-2).css('display','block');
-      }
 });
-// .css({'display': 'block', 'opacity': 0}).animate({'opacity': 1}, 300);
-//
-//
-// .animate({'opacity': 0}, 300, function() {
-//             $(this).css({'display': 'none'})
-
-
-// 5 8 9
-// var clickEle = $('.left-div span');
-// clickEle.click(function () {
-//    var indexNo = $(this).index();
-//    $('.right-div span').eq(indexNo).toggleClass('hide');
-// })
-
-
-
-
-      //
-      // if($('.list_area>li:nth-child(1)').hasClass('active')){
-      //   .all_channel_area > ul>li 전체 오퍼 1
-      //   1,2,3,4,5,6 -2
-      //   -1 1  :전체 op1
-      //    0 2 : 전체 오퍼0, 12
-      //   1 3 : 34
-      //   2 4:45
-      //   3 5:56
-      //   4 6:67
-      //
-      // }
-  });
