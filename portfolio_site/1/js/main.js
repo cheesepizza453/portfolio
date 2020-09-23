a378b480b26bcf630b075b0fc1ec7477f675635d
cheesepizza453 <71148177+cheesepizza453@users.noreply.github.com>
2020-09-23 22:38:14 +0900

    포트폴리오.v.0.0.1.3
    뷰포트 추가(1/2)

--- portfolio_site/1/js/main.js
+++ portfolio_site/1/js/main.js
@@ -14,6 +14,27 @@ $(document).ready(function () {
 	$(window).scrollTop($(thisTarget).offset().top);
 	});
 
+	// 현재 스크롤에 따른 메뉴 활성화 표시
+	$(window).scroll(function(){
+
+		var factcheck_height = $('#factcheck').height();
+		var story_height = $('#story').height();
+		var people_height = $('#people').height();
+		var position = $(window).scrollTop();
+
+		if(position > 0 && position < factcheck_height ) {
+			$('nav li .menu').removeClass('on');
+			$('nav li:nth-child(1) .menu').addClass('on');
+		}else if(position > factcheck_height && position < factcheck_height+story_height) {
+			$('nav li .menu').removeClass('on');
+			$('nav li:nth-child(2) .menu').addClass('on');
+		}else if(position > factcheck_height+story_height && position < factcheck_height+story_height+position) {
+			$('nav li .menu').removeClass('on');
+			$('nav li:nth-child(3) .menu').addClass('on');
+		}
+	});
+
+
 	//이미지 슬라이드
 	$('.slick-items').slick({
 		autoplay : true,
@@ -24,7 +45,7 @@ $(document).ready(function () {
 		arrows: true,
 		slidesToShow: 2,
 		slidesToScroll: 1,
-		fade: false[]
+		fade: false,
 	});
 
 	$('.slick-track').css('width','100%');
