21982550a1bc94c69460363baf240b2ff9bd8321
cheesepizza453 <earlykim1209@gmail.com>
2023-08-13 23:59:30 +0900

    메인페이지 리플로우 개선 위해 쓰로틀링 적용

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,22 +6,36 @@ const gnbMenu1 = document.getElementById("gnb_menu_1");
 const gnbMenu2 = document.getElementById("gnb_menu_2");
 const gnbBg = document.querySelector(".gnb_bg");
 
-window.addEventListener("mousemove", (e) => {
+function throttle(callback, delay) {
+  let timeoutId;
+  return function (...args) {
+    if (!timeoutId) {
+      timeoutId = setTimeout(() => {
+        callback(...args);
+        timeoutId = null;
+      }, delay);
+    }
+  };
+}
+
+const throttledMouseMove = throttle((e) => {
   const x = e.clientX;
   const y = e.clientY;
 
-  // gnbPath.style.left = `${x - gnbPath.offsetWidth / 2}px`;
-  // gnbPath.style.top = `${y - gnbPath.offsetHeight / 2}px`;
-
   gnbPath.style.cssText = `left:${x - gnbPath.offsetWidth / 2}px; top:${y - gnbPath.offsetHeight / 2}px;`;
-});
+}, 16);
 
-gnbMenu1.addEventListener("mouseover", () => {
+window.addEventListener("mousemove", throttledMouseMove);
+
+const throttledMouseOverMenu1 = throttle(() => {
   gnbBg.classList.remove("menu2");
   gnbBg.classList.add("menu1");
-});
+}, 300);
 
-gnbMenu2.addEventListener("mouseover", () => {
+const throttledMouseOverMenu2 = throttle(() => {
   gnbBg.classList.remove("menu1");
   gnbBg.classList.add("menu2");
-});
+}, 300);
+
+gnbMenu1.addEventListener("mouseover", throttledMouseOverMenu1);
+gnbMenu2.addEventListener("mouseover", throttledMouseOverMenu2);
